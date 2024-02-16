e938f8874aba824b08cb45121cb779d4e8bb960d
DashaShevchuk <77dasha0377@gmail.com>
2024-02-16 16:43:24 +0200

    first commit

--- Chrome extension/background.js
+++ Chrome extension/background.js
@@ -3,7 +3,6 @@
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
-    console.log("Text:", message.text);
     console.log("Received conference ID:", message.conferenceId);
     console.log("Conference date", message.conferenceDate);
     console.log("Conference start time: ", message.conferenceStartTime);
@@ -11,19 +10,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     console.log("Status: ", message.status);
     console.log("Duration: ", message.callDuration);
     console.log("Participants: ", message.participants)
-    
-    // // Check if participants is an array before iterating
-    // if (Array.isArray(message.participants)) {
-    //     // Log each participant
-    //     console.log("Participants:");
-    //     message.participants.forEach((participant, index) => {
-    //         console.log(`Participant ${index + 1}:`);
-    //         console.log("  Name:", participant.name);
-    //         console.log("  Image:", participant.image);
-    //     });
-    // } else {
-    //     console.log("Participants data is not an array or is undefined.");
-    // }
 
     console.log("____________________________");
 });
